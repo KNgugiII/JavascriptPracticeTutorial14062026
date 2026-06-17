e1d44adeb824b5bc583adbd70b5db1587a95e2ea
KenntheAccessVBADev <ngugikenn@gmail.com>
2026-06-18 00:25:43 +0300

    Arithmetic Operators

---
+++ JAVASCRIPT Code/ArithmeticOperators.js
@@ -0,0 +1,51 @@
+/* Arithmetic operators consist of Operands and Operators in a mathematical calculation
+Operands refer to variables and values
+Operators include but are not limited to (+,-,/,%,*,**) */
+    
+    let numStudents=30;
+
+    /* numStudents= numStudents+1;  */     /* Illustrates the ADDITION operator */
+
+    /* numStudents= numStudents-1;  */     /* Illustrates the SUBTRACTION operator */
+
+   /*  numStudents= numStudents/2; */     /* Illustrates the DIVISION operator */
+
+    /* numStudents= numStudents%4; */     /* Illustrates the MOD operator, ie shows the remainder */
+
+    /* numStudents= numStudents*2; */      /* Illustrates the MULTIPLICATION operator */
+
+    /* numStudents= numStudents**2  */     /* Illustrates the EXPONENT operator, the power of */
+
+    
+    /* Augmented Assigment Operators - these refer to the SHORTHAND METHOD of the above. See below; */
+
+    /* numStudents+=1 */;     /* Illustrates the ADDITION operator */
+
+    /* numStudents-=1; */     /* Illustrates the SUBTRACTION operator */
+
+    /* numStudents/=2; */     /* Illustrates the DIVISION operator */
+
+    /* numStudents*=2; */     /* Illustrates the MULTIPLICATION operator */
+
+    /* numStudents%=4; */     /* Illustrates the MOD operator */
+
+    /* numStudents**=2; */     /* Illustrates the EXPONENT operator */
+
+    /* Increment and Decrement Operators */
+    /* numStudents++; */       /* Illustrates how to ADD A SINGLE 1 to the variable */
+    /* numStudents--; */       /* Illustrates how to DEDUCT A SINGLE 1 to the variable */
+
+    /* Operator Precedence - this refers to the ORDER in which a calculation should be done */
+    /*
+    To remember the Operator Precedence, use
+        P - parenthesis ()
+        E - exponent Power Of which is illustrated by 2 multiplication/asterix symbols
+        M - module
+        D - division
+        A - addition
+        S - subtraction
+    */
+
+
+    console.log(numStudents);
+    
