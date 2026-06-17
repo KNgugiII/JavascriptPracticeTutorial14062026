89a2603dcfa2b2f7e924383ca8d0d32c072bc231
KenntheAccessVBADev <ngugikenn@gmail.com>
2026-06-17 22:05:38 +0300

    JS HTML CSS Intro

---
+++ JAVASCRIPT Code/IntroIndex.js
@@ -0,0 +1,20 @@
+/* The document 'getElementbyId' property is used to target a specific HTML element by using its unique ID.
+The textContent property is used to CHANGE THE TEXT CONTENT of the identified HTML element 
+
+############OBSERVATION: BE MINDFUL OF THE CASE OF THE PROPERTIES.############
+
+*/
+
+document.getElementById('hdrPrimaryHeader').textContent='JAVASCRIPT INTRODUCTION'
+
+document.getElementById('prprghIntroParagraph001').textContent=`Mambo Vipi Wadau?`
+
+document.getElementById('prprghIntroParagraph002').textContent="Poa sana Chairman"
+
+document.getElementById('prprghIntroParagraph003').textContent='Hali vipi'
+
+/* The below creates an ALERT, ie a windows pop up akin to the msgbox in VBA */
+window.alert(`Goodmorning`)
+
+window.alert(`Goodevening`)
+
