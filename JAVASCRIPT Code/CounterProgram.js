952961bd45017ebcebdd28f888db20f2c3c4b378
KenntheAccessVBADev <ngugikenn@gmail.com>
2026-06-20 02:32:31 +0300

    CounterProg BroCode Assesment

--- JAVASCRIPT Code/CounterProgram.js
+++ JAVASCRIPT Code/CounterProgram.js
@@ -32,3 +32,13 @@ document.getElementById('idDecreaseBttn').onclick=function(){
         document.getElementById('prpgrphCounterVariable').value=varBaseValue -=1;
    //end if
 }
+
+/* Observations:
+BroCode did not use a textbox - he used a label with a predefined value.
+The manner in which he created object variables is Smart. For instance, 
+ const CountLabel = document.getElementById('CountLabel'); or whereupon, if he wanted to assign a value, he'd do as below;
+ CountLabel.textContent=123;
+ const ResetBttn = document.getElementById('BttnReset'); ---when it comes to buttons, the events are captured succintly as below;
+ ResetBttn.onlick=function(){
+     CountLabel.textContent=123;
+ } */
